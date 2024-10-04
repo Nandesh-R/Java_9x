9fa37d5ed4d391ed04e0425d676b30b06ec0fe13
Nandesh Ramachandra <Nandesh_Ramachandr@dellteam.com>
2024-10-04 15:06:05 +0530

    Adding new file

--- src/ex_03102024/task1.java
+++ src/ex_03102024/task1.java
@@ -1,6 +1,6 @@
 package ex_03102024;
 
-public class task {
+public class task1 {
     public static void main(String[] args) {
         System.out.println("10*1=10");
         System.out.println("10*2=20");
--- src/ex_03102024/task2.java
+++ src/ex_03102024/task2.java
@@ -1,4 +1,10 @@
 package ex_03102024;
 
 public class task2 {
+    public static void main(String[] args) {
+        System.out.println(10+2);//Add
+        System.out.println(10-2);//Sub
+        System.out.println(10*2);//Mul
+        System.out.println(10/2);//Div
+    }
 }
