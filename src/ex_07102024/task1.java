81a7483a22f432d2724b19eec52bc50a38f42eb0
Nandesh Ramachandra <Nandesh_Ramachandr@dellteam.com>
2024-10-07 20:30:00 +0530

    Difference bw print & println

---
+++ src/ex_07102024/task1.java
@@ -0,0 +1,15 @@
+package ex_07102024;
+
+public class task1 {
+    public static void main(String[] args) {
+        /*Difference bw print & println
+        println > This method outputs the text and then moves the cursor to the next line.
+        Any subsequent output will start on a new line.
+        print >  This method outputs the text but does not move to a new line afterward.
+        Subsequent output will continue on the same line.
+         */
+
+        System.out.println("What is your name:");
+        System.out.print("Nandesh");
+    }
+}
