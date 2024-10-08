88fc5d1680c32f9f707685f7abde6d9cf1c1356b
Nandesh Ramachandra <Nandesh_Ramachandr@dellteam.com>
2024-10-08 18:51:53 +0530

    lab001 on 08 Oct 24 Include Data type usage

---
+++ src/ex_08102024/lab001.java
@@ -0,0 +1,11 @@
+package ex_08102024;
+
+public class lab001 {
+    public static void main(String[] args) {
+        int age = 30;
+        double height = 5.6;
+        System.out.println("Nandesh age is:" + age);
+        System.out.println("Nandesh height is:" + height);
+
+    }
+}
